837744ae5e0a8a341e9044030ca623e173fa5d75
Tim Hoerner <thoerner@gmail.com>
2023-07-28 22:46:18 -0400

    update font

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,17 @@ const Main = ({ isMobile }) => {
         />
       </div>
       <div className="card" style={{width: '300px', height: '300px', display: 'flex', alignItems: 'center'}}>
-        <p style={{margin: '0 auto'}}>Lazy Butts GIF</p>
+        <span style={{margin: '0 auto'}}>Lazy Butts GIF</span>
       </div>
       <div className="card">
         <h1>Behind the Tail</h1>
         <h2>The Lazy Butts Story</h2>
-        <p style={{ fontFamily: "REM" }}>Welcome to the extraordinary kingdom of the 3D Kings! We are a group of playful and mighty Lazy Lions, who've ventured far beyond the savannah and into the realm of the NFTs!</p>
-        <p style={{ fontFamily: "REM" }}>In our dynamic jungle, we are not just about the roars, the mane, or the striking, swaggering stride; we're about the whole lion! So, we thought - why stop at the top half? Let's give our pride something to truly ROAR about! And thus, the Lazy Butts NFT was born.</p>
-        <p style={{ fontFamily: "REM" }}>As the 3D Kings, we understand that every lion in our pride is a complete individual - from the tip of their majestic mane right down to their unique and expressive...well...butts. We realized our community was missing something crucial: a bottom half!</p>
-        <p style={{ fontFamily: "REM" }}>Lazy Butts NFT is our fun and quirky answer to the partial portrayal of our mighty lions. We decided it was high time we extended our majesty to encompass our whole selves! It's our way of adding a little bit more lion to the pride, bringing some levity and humor to the blockchain, and creating a fuller, well-rounded NFT experience.</p>
-        <p style={{ fontFamily: "REM" }}>Each Lazy Butt NFT is an unofficial extension designed especially for the Lazy Lion NFT holders. And just like our original Lazy Lions, these unique extensions are as diverse and distinctive as the lions in our pride. Whether your lion has a shiny, glittering bottom or a rugged, warrior's wear, the Lazy Butts NFT lets you express your full lion-ness in a whole new way.</p>
-        <p style={{ fontFamily: "REM" }}>So come join the hilarity, embrace the charm, and let's ROAR with laughter together! Adopt a Lazy Butt today, and show the world that your Lazy Lion truly is the king of the NFT jungle, from top to bottom!</p>
+        <p>Welcome to the extraordinary kingdom of the 3D Kings! We are a group of playful and mighty Lazy Lions, who've ventured far beyond the savannah and into the realm of the NFTs!</p>
+        <p>In our dynamic jungle, we are not just about the roars, the mane, or the striking, swaggering stride; we're about the whole lion! So, we thought - why stop at the top half? Let's give our pride something to truly ROAR about! And thus, the Lazy Butts NFT was born.</p>
+        <p>As the 3D Kings, we understand that every lion in our pride is a complete individual - from the tip of their majestic mane right down to their unique and expressive...well...butts. We realized our community was missing something crucial: a bottom half!</p>
+        <p>Lazy Butts NFT is our fun and quirky answer to the partial portrayal of our mighty lions. We decided it was high time we extended our majesty to encompass our whole selves! It's our way of adding a little bit more lion to the pride, bringing some levity and humor to the blockchain, and creating a fuller, well-rounded NFT experience.</p>
+        <p>Each Lazy Butt NFT is an unofficial extension designed especially for the Lazy Lion NFT holders. And just like our original Lazy Lions, these unique extensions are as diverse and distinctive as the lions in our pride. Whether your lion has a shiny, glittering bottom or a rugged, warrior's wear, the Lazy Butts NFT lets you express your full lion-ness in a whole new way.</p>
+        <p>So come join the hilarity, embrace the charm, and let's ROAR with laughter together! Adopt a Lazy Butt today, and show the world that your Lazy Lion truly is the king of the NFT jungle, from top to bottom!</p>
       </div>
       <div className="card">
         <h2>What's in a Butt?</h2>
@@ -35,7 +35,7 @@ const Main = ({ isMobile }) => {
         <button>Claim my Butts</button>
       </div>
       <div className="card" style={{width: '500px', height: '300px', display: 'flex', alignItems: 'center'}}>
-        <p style={{margin: '0 auto'}}>Infographic of mint funds allocation</p>
+        <span style={{margin: '0 auto'}}>Infographic of mint funds allocation</span>
       </div>
       <div className="card">
         <h2>Embrace the Full ROAR of Your Lion with <span style={{fontSize: 'larger'}}>Lazy Butts</span>!</h2>
