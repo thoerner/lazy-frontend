3327c1903f928198a1452f1486b954480c8d78ef
Tim Hoerner <thoerner@gmail.com>
2023-07-28 20:23:14 -0400

    update formatting

--- src/App.jsx
+++ src/App.jsx
@@ -36,13 +36,15 @@ const Main = ({ isMobile }) => {
         Infographic of mint funds allocation
       </div>
       <div className="card">
-        <h3>Embrace the Full ROAR of Your Lion with Lazy Butts! 🦁🍑</h3>
+        <h3>Embrace the Full ROAR of Your Lion with <span style={{fontSize: 'larger'}}>Lazy Butts</span>! </h3>
+        <p style={{fontSize: '25px'}}>🦁 🍑</p>
         <p>What embodies the spirit of a lion? Is it the majestic stride, the wind rustling through the mane, or the striking, earth-shaking roar? We believe it's all about the Pride! And in our diverse Pride of Lazy Lions, we've made a thunderous #ROAR that echoes through the NFT landscape.</p>
         <p>Our ever-passionate Lazy Lion owners simply can't get enough of showing off their kings of the jungle. From vibrant illustrations to awe-inspiring derivatives, the creativity and joy our community expresses through their lions is truly inspiring.</p>
         <p>As the 3D Kings, we're always looking out for our Pride. We saw an opportunity to help you express your lion even more boldly, extending its splendor and ROAR! Our vision? To provide a new dimension of expression and opportunity for our community members to enhance their unique IP.</p>
         <p>Blessed with a troupe of incredibly talented individuals in our Pride, we devised a plan to offer a low-cost claim for all lion holders, all while supporting the growth of the 3D Kings community. A significant portion of the funds raised through Lazy Butts will go directly to the 3D Kings Community Wallet, further fueling our collective efforts.</p>
         <p>Everyone who had a paw in crafting the Lazy Butts project is a proud owner of a 3D Lazy NFT. It's worth noting, however, that while we all are a part of the Lazy Lions community, Lazy Butts is not officially affiliated with the Lazy Lions project.</p>
-        <p>Lazy Butts is an independent venture, created by lions, for lions. A delightful, full-bodied expression of our love for our Lazy Lions, and a testament to the creativity, fun, and camaraderie within our Pride. Embrace the ROAR, adopt a Lazy Butt today! 🦁👑🎉</p>
+        <p>Lazy Butts is an independent venture, created by lions, for lions. A delightful, full-bodied expression of our love for our Lazy Lions, and a testament to the creativity, fun, and camaraderie within our Pride. Embrace the ROAR, adopt a Lazy Butt today!</p>
+        <p style={{fontSize: '50px'}}>🦁 👑 🎉</p>
       </div>
       <div className="card">
         <TeamInfo />
