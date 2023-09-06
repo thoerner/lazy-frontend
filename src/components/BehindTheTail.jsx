c888a6d4cc3ffd4391574a05e06e12b675c5a453
Tim Hoerner <thoerner@gmail.com>
2023-09-06 12:56:38 -0400

    update behind the tail

--- src/components/BehindTheTail.jsx
+++ src/components/BehindTheTail.jsx
@@ -3,10 +3,19 @@ const BehindTheTail = () => {
         <div className="card">
             <h1>Behind the Tail</h1>
             <h2>The Lazy Butts Story</h2>
-            <p>As the 3D Kings, we understand that every lion in our pride is a complete individual - from the tip of their majestic mane right down to their unique and expressive...well...butts. We realized our community was missing something crucial: a bottom half!</p>
-            <p>Lazy Butts NFT is our fun and quirky answer to the partial portrayal of our mighty lions. We decided it was high time we extended our majesty to encompass our whole selves! It's our way of adding a little bit more lion to the pride, bringing some levity and humor to the blockchain, and creating a fuller, well-rounded NFT experience.</p>
-            <p>Each Lazy Butt NFT is an unofficial extension designed especially for the Lazy Lion NFT holders. And just like the original Lazy Lions, these unique extensions are as diverse and distinctive as the lions in our pride. The Lazy Butts NFT lets you express your full lion-ness in a whole new way.</p>
-            <p>So come join the hilarity, embrace the charm, and let's ROAR with laughter together! Adopt a Lazy Butt today, and show the world that your Lazy Lion truly is the king of the NFT jungle, from head to tail!</p>
+            <p>As the 3D Kings, we know each lion in our pride is a glorious individual—majestic from the tip of their mane to the, well, let's say, unique characteristics of their backside. It dawned on us that something was missing from our NFT pride: the other "half" of our lionhood. Enter Lazy Butts NFTs, the yin to our mane's yang, the behind-the-scenes star that deserved its own spotlight.</p>
+            <p>We wanted to broaden the scope of our majestic aura by honoring our lion-ness in its entirety. Lazy Butts NFT is our whimsical yet considered response to the partial portrayal of our regal Lazy Lions. In the same vein as the original Lazy Lions, Lazy Butts offers a dash of humor, light-heartedness, and of course, more individuality to the blockchain world. It's our endeavor to create a fuller, richer, and funnier NFT experience for all.</p>
+            <h3>Attention to Details: A Tail-Tale Sign of Quality</h3>
+            <p>What sets Lazy Butts apart is not just the novelty but the craftsmanship that goes into each NFT. We are a team of Lazy Lion community members, but rest assured, we are anything but lazy when it comes to creativity. Each Lazy Butt NFT is designed as a complementary extension to its corresponding Lazy Lion. The intricate attributes of Lazy Butts align perfectly with those of their majestic counterparts:</p>
+            <ul>
+                <li><strong>Headgear to Accessories:</strong> Your Lazy Lion's crown or LAZY hat translates seamlessly into quirky Lazy Butt accessories. Think of it as the flair your lion's rear end deserves.</li>
+                <li><strong>Body to Butt:</strong> The colors, markings, or even scars on your Lazy Lion's body won't be left behind—they'll find their way onto the Lazy Butt as well. Whether your lion has zebra-like stripes or unique cosmic swirls, the Lazy Butt ensures that the artistry carries through to the tail end, making your NFT a well-rounded masterpiece.</li>
+                <li><strong>Bodygear to Bodygear Bottom:</strong> A gladiators's armor or a ranger shirt on your Lazy Lion? Expect the same high-fashion on the Lazy Butt, only tailored for the bottom half. It's the ensemble you never knew you needed.</li>
+                <li><strong>Mane to Tail Tuft:</strong> Whether your lion has an ice mane or a rainbow-colored extravaganza, the tail tuft on your Lazy Butt will carry through that same distinctive style. Your tail tuft can now wag with as much pride as your mane.</li>
+                <li><strong>Background to Butt Background:</strong> Whether your Lazy Lion sports a khaki background symbolizing the savanna or a copper hue that resonates with the earth's core, your Lazy Butt will maintain this backdrop. This ensures a consistent and harmonious aesthetic across your full NFT lion experience.</li>
+            </ul>
+            <p>So why settle for a half-told story when you can complete your lion's lore? With Lazy Butts, you get to make a full-circle statement in the ever-evolving NFT space.</p>
+            <p>Let's bring more ROARs of laughter, charm, and whimsy to our community. Adopt a Lazy Butt today, and prove that your Lazy Lion is the undisputed king of the NFT jungle, from mane to tail!</p>
         </div>
     )
 }
