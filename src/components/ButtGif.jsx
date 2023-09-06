36389beb8a076de31f41028a207e63b3d14f913a
Tim Hoerner <thoerner@gmail.com>
2023-09-06 13:20:52 -0400

    fix header image size on mobile

--- src/components/ButtGif.jsx
+++ src/components/ButtGif.jsx
@@ -2,7 +2,7 @@ import LBHeader from '../assets/LB-header2.jpg'
 
 const ButtGif = ({isMobile}) => {
     return (
-        <div className="card">
+        <div className={`card ${isMobile ? 'mobile-gif-card' : null}`}>
             <img src={LBHeader} className="header-image" alt="Header image" />
         </div>
     )
