21ac80230c8b1ac3917f4d9817decfeb6be707e3
Tim Hoerner <thoerner@gmail.com>
2023-09-15 11:02:55 -0400

    update snapshot date

--- src/components/EarlyAccessMessage.jsx
+++ src/components/EarlyAccessMessage.jsx
@@ -24,12 +24,12 @@ const EarlyAccessMessage = ({ address, isAllowListed }) => {
                         <h2 style={{ color: '#00caf8bb', margin: '0' }}>Early&nbsp;Access</h2>
                         <h2 style={{ color: '#ff2b06bb', margin: '0', transform: 'translateY(-90%) translateX(1%)' }}>Early Access</h2>
                         <p style={{ marginTop: '0', zIndex: 10, fontSize: '0.8rem' }}>3D Glasses NFT holders, including both Lazy Lions and Lazy Cubs, will receive early access! You can claim as many Lazy Butts as you wish, 24 hours before the official launch. Plus, your first NFT claimed comes with a 50% discount!</p>
-                        <p style={{ marginTop: '0', zIndex: 10, fontSize: '0.8rem' }}>Snapshot taken: 9/1/2023</p>
+                        <p style={{ marginTop: '0', zIndex: 10, fontSize: '0.8rem' }}>Snapshot taken: 9/14/2023 @ 5:00pm EDT</p>
                     </> : <>
                         <h1 style={{ color: '#00caf8bb', margin: '0' }}>Early&nbsp;Access</h1>
                         <h1 style={{ color: '#ff2b06bb', margin: '0', transform: 'translateY(-90%) translateX(1%)' }}>Early Access</h1>
                         <p style={{ marginTop: '0', zIndex: 10 }}>3D Glasses NFT holders, including both Lazy Lions and Lazy Cubs, will receive early access! You can claim as many Lazy Butts as you wish, 24 hours before the official launch. Plus, your first NFT claimed comes with a 50% discount!</p>
-                        <p style={{ marginTop: '0', zIndex: 10 }}>Snapshot taken: 9/1/2023</p>
+                        <p style={{ marginTop: '0', zIndex: 10 }}>Snapshot taken: 9/14/2023 @ 5:00pm EDT</p>
                     </>}
 
             </div>
