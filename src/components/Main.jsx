bfe0c971f607e9d8552bbf3c28a54f0e736cf6dc
Tim Hoerner <thoerner@gmail.com>
2023-09-14 21:52:27 -0400

    add mint announcement

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,12 +10,26 @@ import Disclaimer from './Disclaimer.jsx'
 import TeamStatements from './TeamStatements.jsx'
 import AttentionToDetails from './AttentionToDetails.jsx'
 
+const MintAnnouncement = () => {
+    return (
+        <div className="mint-announcement">
+            <div className="mint-announcement-text">
+                <h2>Early Access Claim on 9/22! Public Claim on 9/23!</h2>
+                <p>
+                    See the <a href="https://x.com/3DKingsNFT/status/1438237702025076224" target="_blank" rel="noreferrer">X announcement</a> for more details.
+                </p>
+            </div>
+        </div>
+    )
+}
+
 const Main = ({ isMobile }) => {
     return (
         <div className="main">
             <ButtLogo isMobile={isMobile} />
             <Disclaimer />
             <ButtGif isMobile={isMobile} />
+            <MintAnnouncement />
             <BehindTheTail />
             <WhatsInAButt isMobile={isMobile} />
             <AttentionToDetails />
