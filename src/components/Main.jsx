80b4fa518a9252b078a874d6b2b0f5c17761a89e
Tim Hoerner <thoerner@gmail.com>
2023-09-14 21:53:59 -0400

    move mint announcement to own component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,19 +9,7 @@ import TeamSection from './TeamSection.jsx'
 import Disclaimer from './Disclaimer.jsx'
 import TeamStatements from './TeamStatements.jsx'
 import AttentionToDetails from './AttentionToDetails.jsx'
-
-const MintAnnouncement = () => {
-    return (
-        <div className="mint-announcement">
-            <div className="mint-announcement-text">
-                <h2>Early Access Claim on 9/22! Public Claim on 9/23!</h2>
-                <p>
-                    See the <a href="https://x.com/3DKingsNFT/status/1438237702025076224" target="_blank" rel="noreferrer">X announcement</a> for more details.
-                </p>
-            </div>
-        </div>
-    )
-}
+import MintAnnouncement from './MintAnnouncement.jsx'
 
 const Main = ({ isMobile }) => {
     return (
---
+++ src/components/MintAnnouncement.jsx
@@ -0,0 +1,14 @@
+const MintAnnouncement = () => {
+    return (
+        <div className="mint-announcement">
+            <div className="mint-announcement-text">
+                <h2>Early Access Claim on 9/22! Public Claim on 9/23!</h2>
+                <p>
+                    See the <a href="https://x.com/3DKingsNFT/status/1438237702025076224" target="_blank" rel="noreferrer">X announcement</a> for more details.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default MintAnnouncement
